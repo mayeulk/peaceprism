f382f2e795d18ae9334d860a80b2c4a047096ad5
Mayeul Kauffmann <mayeulk@users.noreply.github.com>
2008-03-11 09:23:55 +0000

    modif 108

--- projet_essai2/app/controllers/application.rb
+++ projet_essai2/app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+# modif 108
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '0adcd9647322b3fc06ef4d7659be79c2'
