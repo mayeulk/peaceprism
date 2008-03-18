94a71e5ea4b1c7b82780b0c5d7aaf6ce88fe0698
gtokarski <guytokarski@gmail.com>
2008-03-18 11:02:29 +0000

    nouvelle carte pour mozilla, appelée par list.html.erb, générée par la requete /script/postgis_vers_svg.sql

--- carte_curseur_v1/script/postgis_vers_svg.sql
+++ carte_curseur_v1/script/postgis_vers_svg.sql
@@ -1,5 +1,5 @@
 -- NB: SOUS PGADMIN", POUR EXPORTER LE RESULTAT DE CE FICHIER, CHOISIR LE MENU FICHIER/PREFERENCES/REQUETES
--- PUIS: Nombre max. de caractères par colonne: 100000
+-- PUIS: Nombre max. de caracteres par colonne: 100000
 
 -- en tete du fichier .svg
 drop table tmp;
@@ -10,7 +10,7 @@ select '<svg id="monde" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www
 union all
 -- carte du monde en l'an 2000
 select  '<path id="' || replace(cntry_name, '&', 'and') -- le caractere "&" plante en svg
-||'" fill="lightgray" stroke="#000000" stroke-width="0.25" d=" '
+||'" fill="lightgray" stroke="#000000" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" '
  || assvg(Scale(Simplify(the_geom, 10000),0.00001,0.00001,1),0,1)||'"/>' as svg
 -- assvg(Scale(Simplify(the_geom,echelle_de_simplif.),chgmt_echelle_x,chgmt_echelle_y,?),0,1)
 -- apparemment, simplifier les frontieres joue aussi sur la vitesse de demasquage des zones de conflit
@@ -20,8 +20,8 @@ union all
 select '<g id="conflits" visibility="hidden">'
 union all
 
-select  '<path id="lt'::text||lat||'lg'|| lon||'rd'||radius||'" visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" d=" '
- || assvg(Scale(the_geom,0.00001,0.00001,1),0,1)||'"/>' as svg
+select  '<path id="lt'::text||lat||'lg'|| lon||'rd'||radius||'" visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" '
+ || assvg(Scale(Simplify(the_geom, 10000),0.00001,0.00001,1),0,1)||'"/>' as svg
 --,lat ,  lon ,  radius, confid, assvg(the_geom,0,0)
 from conflits_ext
 group by lat,lon,radius, the_geom
@@ -30,5 +30,5 @@ union all
 -- queue du fichier .svg
 select '"</g></svg>'
 );
-copy tmp to '/home/commun/ecole/REC/PRISM/tmp/_guy_carte_moz.rhtml';
+copy tmp to '/home/commun/ecole/REC/PRISM/tmp/guy_carte_moz.svg';
 -- copy tmp to '/home/gtokarski/guy_carte_moz.rhtml';
