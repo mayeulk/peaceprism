de5a00ece9ab2473981466a80fa6c51c365cca8d
gtokarski <guytokarski@gmail.com>
2008-03-27 14:53:31 +0000

    sauvegarde version fonctionnelle avec les requetes qui generent les partiels svg et vml a la meme echelle

--- carte_curseur_v1/script/postgis_vers-vml.sql
+++ carte_curseur_v1/script/postgis_vers-vml.sql
@@ -7,14 +7,14 @@ drop table tmp ;
 create table tmp as
 (
 -- select E'<html xmlns:v\\="urn:schemas-microsoft-com:vml"><head><style> v:* { behavior: url(#default#VML); } </style> </head><body>
-select '<v:group id="monde" style="position:relative;left:-43px;top:-11px; width:120px;height:60px;"  coordsize="600,300" coordorigin="-600,-300">'
--- <v:rect style="height:400; width:400;" fillcolor="#B4DAF6" />' -- la mer en bleu pale
+select '<v:group id="monde" style="position:relative;left:0px;top:0px; width:600px;height:300px;"  coordsize="3600,1800" coordorigin="0,0">'
+|| '<v:rect style="top:-900; left:-1800; height:1800; width:3600;" fillcolor="#B4DAF6" />' -- la mer en bleu pale
 union all
 
 -- carte du monde en l'an 2000 :
 -- d'abord les grands pays (>2000 km2), tres simplifies
-select  '<v:shape id="' || replace(cntry_name, '&', 'and') 
-|| '" style="position: relative; width:600;height:300" fillcolor="#D3D3D3" strokecolor="#000000" strokeweight="0.25pt" onmousedown="survol_zone(this.id);" path=" '
+select '<v:shape id="' || replace(cntry_name, '&', 'and') 
+|| '" style="position: relative; width:3600;height:1800" fillcolor="#D3D3D3" strokecolor="#000000" strokeweight="0.25pt" onmousedown="survol_zone(this.id);" path=" '
 || regexp_replace(regexp_replace(assvg(Scale(Simplify(the_geom,10000),0.0001,0.0001,1),0,0), 'M', 'm', 'g'), 'm +(-?[0-9]+) +(-?[0-9]+) +(-?[0-9]+)', E'm \\1 \\2 l \\3', 'g') ||'"/>' as svg
 -- par rapport a  la ligne ci-dessous, on a multiplie par 10 et enleve la decimale car IE6 VML confond le point decimal avec un separateur de coordonnees...
 -- || assvg(Scale(Simplify(the_geom,50000),0.00001,0.00001,1),0,1)||'"/>' as svg
@@ -24,14 +24,14 @@ from world where world.begin<=2000 and world.end>=2000 and (area2d(the_geom)/100
 union all
 -- puis les petits pays (<2000 km2), sans simplification
 select  '<v:shape id="' || replace(cntry_name, '&', 'and') 
-|| '" style="position: relative; width:600;height:300" fillcolor="#D3D3D3" strokecolor="#000000" strokeweight="0.1pt" onmousedown="survol_zone(this.id);" path=" '
+|| '" style="position: relative; width:3600;height:1800" fillcolor="#D3D3D3" strokecolor="#000000" strokeweight="0.1pt" onmousedown="survol_zone(this.id);" path=" '
 || regexp_replace(regexp_replace(assvg(Scale (the_geom,0.0001,0.0001,1),0,0), 'M', 'm', 'g'), 'm +(-?[0-9]+) +(-?[0-9]+) +(-?[0-9]+)', E'm \\1 \\2 l \\3', 'g') ||'"/>' as svg
 from world where (world.begin<=2000 and world.end>=2000) and (area2d(the_geom)/1000000)<=2000
 union all
 
 -- zones de conflits :
-select  '<v:shape id="lt'::text||lat||'lg'|| lon||'rd'||radius
-||'" style="position:relative; width:600; height:300; visibility:hidden" fillcolor="red" strokecolor="black" strokeweight="0.25pt" onmousedown="survol_zone(this.id);" path=" '
+select '<v:shape id="lt'::text||lat||'lg'|| lon||'rd'||radius
+|| '" style="position:relative; width:3600; height:1800; visibility:hidden" fillcolor="#F08080" strokecolor="#B22222" strokeweight="0.25pt" onmousedown="survol_zone(this.id);" path=" '
 || regexp_replace(regexp_replace(assvg(Scale(Simplify(the_geom,10000),0.0001,0.0001,1),0,0), 'M', 'm', 'g'), 'm +(-?[0-9]+) +(-?[0-9]+) +(-?[0-9]+)', E'm \\1 \\2 l \\3', 'g') ||'"/>' as svg
 from conflits_ext
 group by lat,lon,radius, the_geom
