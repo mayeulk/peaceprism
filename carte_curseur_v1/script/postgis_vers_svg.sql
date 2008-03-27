a77adb76f2b1c0ab2d997d2e893cba063cd45690
gtokarski <gtokarski@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-03-27 14:53:31 +0000

    sauvegarde version fonctionnelle avec les requetes qui generent les partiels svg et vml a la meme echelle

--- carte_curseur_v1/script/postgis_vers_svg.sql
+++ carte_curseur_v1/script/postgis_vers_svg.sql
@@ -3,15 +3,29 @@
 
 -- NB: SOUS PGADMIN", POUR EXPORTER LE RESULTAT DE CE FICHIER, CHOISIR LE MENU FICHIER/PREFERENCES/REQUETES
 -- PUIS: Nombre max. de caracteres par colonne: 100000
+drop view test_geom;
+create view test_geom as
+(
+-- d'abord les grands pays (>2000 km2), tres simplifies : couleur  grise (lightgrey et black)
+select cntry_name, Scale(Simplify(the_geom, 10000),0.0001,0.0001,1) as the_geom
+-- assvg(Scale(Simplify(the_geom,echelle_de_simplif.),chgmt_echelle_x,chgmt_echelle_y,?), 0:absolute 1:relative,precision)
+-- apparemment, simplifier les frontieres joue aussi sur la vitesse de demasquage des zones de conflit
+from world where (world.begin<=2000 and world.end>=2000) and (area2d(the_geom)/1000000)>2000
+union all
+-- puis les petits pays (<2000 km2), sans simplification
+select cntry_name, Scale (the_geom,0.0001,0.0001,1) as the_geom
+from world where (world.begin<=2000 and world.end>=2000) and (area2d(the_geom)/1000000)<=2000
+);
+
 
 drop table tmp;
 create table tmp as
 (
 -- en tete du fichier .svg
-select '<svg id="monde" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" width="600" height="400" viewBox="-130 -85 300 200" stroke-linejoin="round" stroke-linecap="round">'
-|| '<rect x="-200" y="-200" height="400" width="400" fill="#B4DAF6" />"' -- la mer en bleu pale
+select '<svg id="monde" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" width="600" height="300" viewBox="-1800 -900 3600 1800" stroke-width="1" stroke-linejoin="round" stroke-linecap="round">'
+|| '<rect x="-1800" y="-900" height="1800" width="3600" fill="#B4DAF6"/>' -- la mer en bleu pale
 -- des zones ajoutees au debut car il semble qu'elle previennent contre certains bugs
-|| '"<path id="lt23.5lg121rd200"  visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" M 2 -0 2 0.4 1.8 0.8 1.7 1.1 1.4 1.4 1.1 1.7 0.8 1.8 0.4 2 0 2 -0.4 2 -0.8 1.8 -1.1 1.7 -1.4 1.4 -1.7 1.1 -1.8 0.8 -2 0.4 -2 -0 -2 -0.4 -1.8 -0.8 -1.7 -1.1 -1.4 -1.4 -1.1 -1.7 -0.8 -1.8 -0.4 -2 0 -2 0.4 -2 0.8 -1.8 1.1 -1.7 1.4 -1.4 1.7 -1.1 1.8 -0.8 2 -0.4 2 -0 "/>'
+|| '<path id="lt23.5lg121rd200"  visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" M 2 -0 2 0.4 1.8 0.8 1.7 1.1 1.4 1.4 1.1 1.7 0.8 1.8 0.4 2 0 2 -0.4 2 -0.8 1.8 -1.1 1.7 -1.4 1.4 -1.7 1.1 -1.8 0.8 -2 0.4 -2 -0 -2 -0.4 -1.8 -0.8 -1.7 -1.1 -1.4 -1.4 -1.1 -1.7 -0.8 -1.8 -0.4 -2 0 -2 0.4 -2 0.8 -1.8 1.1 -1.7 1.4 -1.4 1.7 -1.1 1.8 -0.8 2 -0.4 2 -0 "/>'
 || '<path id="lt25lg-14rd500"  visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" M 5 0 4.9 1 4.6 1.9 4.2 2.8 3.5 3.5 2.8 4.2 1.9 4.6 1 4.9 0 5 -1 4.9 -1.9 4.6 -2.8 4.2 -3.5 3.5 -4.2 2.8 -4.6 1.9 -4.9 1 -5 0 -4.9 -1 -4.6 -1.9 -4.2 -2.8 -3.5 -3.5 -2.8 -4.2 -1.9 -4.6 -1 -4.9 0 -5 1 -4.9 1.9 -4.6 2.8 -4.2 3.5 -3.5 4.2 -2.8 4.6 -1.9 4.9 -1 5 0 "/>'
 || '<path id="lt1.5lg10rd100"  visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" M 1 -0 1 0.2 0.9 0.4 0.8 0.6 0.7 0.7 0.6 0.8 0.4 0.9 0.2 1 0 1 -0.2 1 -0.4 0.9 -0.6 0.8 -0.7 0.7 -0.8 0.6 -0.9 0.4 -1 0.2 -1 -0 -1 -0.2 -0.9 -0.4 -0.8 -0.6 -0.7 -0.7 -0.6 -0.8 -0.4 -0.9 -0.2 -1 0 -1 0.2 -1 0.4 -0.9 0.6 -0.8 0.7 -0.7 0.8 -0.6 0.9 -0.4 1 -0.2 1 -0 "/>'
 || '<path id="lt39.75lg19.75rd50"  visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" M 0.5 -0 0.5 0.1 0.5 0.2 0.4 0.3 0.4 0.4 0.3 0.4 0.2 0.5 0.1 0.5 0 0.5 -0.1 0.5 -0.2 0.5 -0.3 0.4 -0.4 0.4 -0.4 0.3 -0.5 0.2 -0.5 0.1 -0.5 -0 -0.5 -0.1 -0.5 -0.2 -0.4 -0.3 -0.4 -0.4 -0.3 -0.4 -0.2 -0.5 -0.1 -0.5 0 -0.5 0.1 -0.5 0.2 -0.5 0.3 -0.4 0.4 -0.4 0.4 -0.3 0.5 -0.2 0.5 -0.1 0.5 -0 "/>'
@@ -19,31 +33,20 @@ select '<svg id="monde" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www
 
 union all
 -- carte du monde en l'an 2000 :
--- d'abord les grands pays (>2000 km2), tres simplifies
-select  '<path id="' || replace(cntry_name, '&', 'and') -- le caractere "&" plante en svg
-||'" fill="lightgray" stroke="#000000" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" '
-|| assvg(Scale(Simplify(the_geom, 10000),0.00001,0.00001,1),0,1)||'"/>' as svg
--- assvg(Scale(Simplify(the_geom,echelle_de_simplif.),chgmt_echelle_x,chgmt_echelle_y,?), 0:absolute 1:relative,precision)
--- apparemment, simplifier les frontieres joue aussi sur la vitesse de demasquage des zones de conflit
-from world where (world.begin<=2000 and world.end>=2000) and (area2d(the_geom)/1000000)>2000
-union all
--- puis les petits pays (<2000 km2), sans simplification
-select  '<path id="' || replace(cntry_name, '&', 'and')
-||'" fill="lightgray" stroke="#000000" stroke-width="0.1" onmousedown="survol_zone(this.id);" d=" '
- || assvg(Scale (the_geom,0.00001,0.00001,1),0,1)||'"/>' as svg
-from world where (world.begin<=2000 and world.end>=2000) and (area2d(the_geom)/1000000)<=2000
+select '<path id="' || replace(cntry_name, '&', 'and') -- le caractere "&" plante en svg
+|| '" fill="#D3D3D3" stroke="#000000" onmousedown="survol_zone(this.id);" d=" '
+|| assvg (the_geom, 0, 0) ||'"/>' as svg from test_geom
 
 union all
--- zones de conflits
-select '"<path id="lt'::text||lat||'lg'|| lon||'rd'||radius||'" visibility="hidden" fill="lightcoral" opacity=".65"  stroke="firebrick" stroke-width="0.25" onmousedown="survol_zone(this.id);" d=" '
-|| assvg(Scale(Simplify(the_geom, 10000),0.00001,0.00001,1),0,1)||'"/>"' as svg
+-- zones de conflits : couleurs rouge (lightcoral et firebrick)
 -- creation d'un code pour les zones de conflits : lat, lon, radius, (confid)
-from conflits_ext
+select '<path id="lt'::text||lat||'lg'|| lon||'rd'||radius||'" visibility="hidden" fill="#F08080" opacity=".65"  stroke="#B22222" onmousedown="survol_zone(this.id);" d=" '
+|| assvg(Scale(Simplify(the_geom, 10000),0.0001,0.0001,1),0,0)||'"/>' as svg from conflits_ext
 group by lat,lon,radius, the_geom
 -- order by confid; 
-union all
 -- queue du fichier .svg
-select '"</svg>'
+union all
+select '</svg>'
 );
 copy tmp to '/home/commun/ecole/REC/PRISM/tmp/guy_carte_moz.svg';
 -- creation du fichier SVG
