6dc934fd9149defff6a5324af0d2fc6211b3cd7e
gtokarski <gtokarski@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-04-24 09:56:07 +0000

    recuperation des donnees a afficher dans le controller carte_controller, creation du fichier js de creation des objets. commit avant essai rjs

--- carte_curseur_v1/app/controllers/fips_cow_code_controller.rb
+++ carte_curseur_v1/app/controllers/fips_cow_code_controller.rb
@@ -1,24 +1,20 @@
 class FipsCowCodeController < ApplicationController
+  # importe le fichier '/db/diamonds_country_codes.csv' dans la table SQL 'fips_cow_codes'
   def import
-    FipsCowCode.delete_all
+    FipsCowCode.delete_all #supprimme toutes les lignes existantes
     
     @fips_cow_file=RAILS_ROOT + '/db/diamonds_country_codes.csv'
     require 'csv'
     
-    # lecture des données ligne par ligne
+    # lecture des donnees ligne par ligne
     CSV::Reader.parse(File.open(@fips_cow_file), "\t") do |row|
       ligne = FipsCowCode.new
       ligne.country_name = row[0]
       ligne.cowcode = row[1]
       ligne.fips_cntry = row[2]
       ligne.contcode = row[3]
-      ligne.save
+      ligne.save #enregistre la ligne dans la table SQL
     end
-    
-
-    
-    
-    
     
     render :text => "OK"
   end
