7564b70eb06ced17762dffd343cc528eff4882c4
mk <mk@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-05-30 16:18:37 +0000

    requete un peu amelioree (vitesse, ne pas utiliser les VIEWs) boucles ruby enlevees du controlleur

--- carte_curseur_v1/app/controllers/carte_controller.rb
+++ carte_curseur_v1/app/controllers/carte_controller.rb
@@ -52,92 +52,138 @@ class CarteController < ApplicationController
       
       @var_annee = "dataset_#{@dataset.id}.var#{@dataset.identifieryear_var}"
       @var_code = "dataset_#{@dataset.id}.var#{@dataset.identifierccode1_var}"
+      
+      
+      
+#
+#      # constitution du jeu de donnees a afficher, en creant un code pays
+#      # fips-annees correspondant au code cow pour une date donnee
+#      # (en accord avec la carte) seulement les donnees affichables
+#      @data = ActiveRecord::Base.connection.select_all("
+#        SELECT #{@var_code} as ccode1,
+#          world.fips_cntry || world.begin || world.end as ccode,
+#          #{@var_annee} as year, 
+#          dataset_#{@dataset.id}.var#{@variable.var_id} as data
+#        FROM dataset_#{@dataset.id},  fips_cow_codes,  world
+#        WHERE #{@var_annee} < 2004
+#          and (#{@var_code} = fips_cow_codes.cowcode)
+#          and (fips_cow_codes.fips_cntry = world.fips_cntry)
+#          and (#{@var_annee} >= world.begin
+#          and #{@var_annee} <= world.end)
+#        ORDER BY  #{@var_annee},
+#          world.fips_cntry || world.begin || world.end
+#        ")
+#        
+#      # toutes les annees existantes pour la carte
+#      @annees2 = ActiveRecord::Base.connection.select_all(
+#        "SELECT annee from annees order by annee")
+#      
+#      tab_annees = Array.new()
+#      c = 0
+#      for elt in @annees2
+#        tab_annees[c] = elt['annee']
+#        c = c + 1
+#      end
+#      
+#      # tous les pays de la carte
+#      @pays2 = ActiveRecord::Base.connection.select_all(
+#        "SELECT world.fips_cntry || world.begin || world.end  as ccode
+#        from world order by world.fips_cntry || world.begin || world.end")
+#      
+#      tab_pays = Array.new()
+#      d = 0
+#      for eltt in @pays2
+#        tab_pays[d] = eltt['ccode']
+#        d = d + 1
+#      end      
+#
+#      # recuperation de la table qui repertorie les pays inexistants 
+#      @diese = ActiveRecord::Base.connection.select_all("
+#        SELECT annee, ccode, diese, manquante FROM diese_null
+#        ORDER BY annee, ccode")
+#      
+#      # pour un souci de proprete, on pourra regrouper les deux boucles en une :
+#      # premiere : constitution du tableau de donnees
+#      tableau_donnees = Array.new()
+#      i = 0
+#      j = 0
+#      for t in @diese
+#        if t['diese'].to_i == 1
+#          tableau_donnees[i] = "#"
+#        else
+#          tableau_donnees[i] = "null"
+#        end
+#        
+#        if ((@data[j].values)[2] == t['annee']) and ((@data[j].values)[1] == t['ccode'])
+#          tableau_donnees[i] = (@data[j].values)[3]
+#          j = j + 1
+#        end
+#           
+#        i=i+1
+#      end
+#      
+#      # deuxieme : transformation en un hash de tableaux (plus facile pour l'affichage
+#      tab_do = Hash.new()
+#      com = 0
+#      for ann in tab_annees
+#        compt = 0
+#        ar = Array.new()
+#        for pa in tab_pays
+#          ar[compt] = tableau_donnees[com]
+#          com = com + 1
+#          compt = compt + 1
+#        end
+#        tab_do[ann] = ar
+#      end
+#        
+#      tab_envoi = Hash.new()
+#      
+#      tab_envoi['annees'] = tab_annees
+#      tab_envoi['pays'] = tab_pays
+#      tab_envoi['data'] = tab_do
 
-      # constitution du jeu de donnees a afficher, en creant un code pays
-      # fips-annees correspondant au code cow pour une date donnee
-      # (en accord avec la carte) seulement les donnees affichables
-      @data = ActiveRecord::Base.connection.select_all("
-        SELECT #{@var_code} as ccode1,
-          world.fips_cntry || world.begin || world.end as ccode,
-          #{@var_annee} as year, 
-          dataset_#{@dataset.id}.var#{@variable.var_id} as data
-        FROM dataset_#{@dataset.id},  fips_cow_codes,  world
-        WHERE #{@var_annee} < 2004
-          and (#{@var_code} = fips_cow_codes.cowcode)
-          and (fips_cow_codes.fips_cntry = world.fips_cntry)
-          and (#{@var_annee} >= world.begin
-          and #{@var_annee} <= world.end)
-        ORDER BY  #{@var_annee},
-          world.fips_cntry || world.begin || world.end
-        ")
-        
-      # toutes les annees existantes pour la carte
-      @annees2 = ActiveRecord::Base.connection.select_all(
-        "SELECT annee from annees order by annee")
-      
-      tab_annees = Array.new()
-      c = 0
-      for elt in @annees2
-        tab_annees[c] = elt['annee']
-        c = c + 1
-      end
-      
-      # tous les pays de la carte
-      @pays2 = ActiveRecord::Base.connection.select_all(
-        "SELECT world.fips_cntry || world.begin || world.end  as ccode
-        from world order by world.fips_cntry || world.begin || world.end")
-      
-      tab_pays = Array.new()
-      d = 0
-      for eltt in @pays2
-        tab_pays[d] = eltt['ccode']
-        d = d + 1
-      end      
 
-      # recuperation de la table qui repertorie les pays inexistants 
-      @diese = ActiveRecord::Base.connection.select_all("
-        SELECT annee, ccode, diese, manquante FROM diese_null
-        ORDER BY annee, ccode")
-      
-      # pour un souci de proprete, on pourra regrouper les deux boucles en une :
-      # premiere : constitution du tableau de donnees
-      tableau_donnees = Array.new()
-      i = 0
-      j = 0
-      for t in @diese
-        if t['diese'].to_i == 1
-          tableau_donnees[i] = "#"
-        else
-          tableau_donnees[i] = "null"
-        end
-        
-        if ((@data[j].values)[2] == t['annee']) and ((@data[j].values)[1] == t['ccode'])
-          tableau_donnees[i] = (@data[j].values)[3]
-          j = j + 1
-        end
-           
-        i=i+1
-      end
-      
-      # deuxieme : transformation en un hash de tableaux (plus facile pour l'affichage
-      tab_do = Hash.new()
-      com = 0
-      for ann in tab_annees
-        compt = 0
-        ar = Array.new()
-        for pa in tab_pays
-          ar[compt] = tableau_donnees[com]
-          com = com + 1
-          compt = compt + 1
-        end
-        tab_do[ann] = ar
-      end
-        
-      tab_envoi = Hash.new()
-      
-      tab_envoi['annees'] = tab_annees
-      tab_envoi['pays'] = tab_pays
-      tab_envoi['data'] = tab_do
+
+tab_envoi=ActiveRecord::Base.connection.select_all(
+"SELECT tab.*, data.data, 
+  CASE
+    WHEN data.data IS NOT NULL and tablepaysannees.annee >= tab.begin and
+      tablepaysannees.annee <= tab.end then CAST (data as text)
+    WHEN tablepaysannees.ccode IS NOT NULL  and tablepaysannees.annee >= tab.begin and
+      tablepaysannees.annee <= tab.end
+      then 'TRUE'
+    ELSE '#'
+  END
+
+from (SELECT distinct annees.annee, world.ccode,
+world.begin, world.end
+        FROM annees, world
+        WHERE annee >= 1950 --(select min(dataset_6.var5) as min from dataset_6)
+          and annee <= 2003 --(select max(dataset_6.var5) as max from dataset_6)
+        ORDER BY  annee, world.ccode) as tab
+
+left join (SELECT dataset_6.var2 as ccode1,world.fips_cntry,
+ world.ccode,
+ dataset_6.var5 as annee,
+ dataset_6.var8 as data
+ FROM dataset_6, fips_cow_codes, world
+ WHERE  (dataset_6.var2 = fips_cow_codes.cowcode)
+ and (fips_cow_codes.fips_cntry = world.fips_cntry)
+ and (dataset_6.var5 >= world.begin
+ and dataset_6.var5 <= world.end)
+ ORDER BY dataset_6.var5,
+ world.ccode) as data
+    ON (tab.annee = data.annee and tab.ccode=data.ccode)
+       left join tablepaysannees ON (tab.ccode=tablepaysannees.ccode
+    and tab.annee = tablepaysannees.annee)
+
+ORDER BY tab.ccode, tab.annee, ccode
+;
+
+"
+
+)
+
 
       render :json => tab_envoi.to_json
 
--- carte_curseur_v1/script/left_join_postgis2json_sans_view.sql
+++ carte_curseur_v1/script/left_join_postgis2json_sans_view.sql
@@ -1,4 +1,17 @@
 /*
+CREATE INDEX dataset_6_var5_idx
+   ON dataset_6 (var5);
+CREATE INDEX dataset_6_var2_idx
+   ON dataset_6 (var2);
+
+alter table world add column ccode text;
+update world
+  SET ccode = world.fips_cntry || world.begin || world.end;
+
+CREATE INDEX world_ccode_idx
+   ON world (ccode);
+
+
 CREATE table tablepaysannees AS
 (
  SELECT world.fips_cntry || world.begin || world.end as ccode, annee 
@@ -16,28 +29,33 @@ world.begin, world.end
           and annee <= 2006 --(select max(dataset_6.var5) as max from dataset_6)
         ORDER BY  annee, world.fips_cntry || world.begin || world.end
 );
+
+CREATE INDEX tabletab_ccode_idx
+   ON tabletab (ccode);
+
+
 */
 
 
 SELECT tab.*, data.data, -- paysannees.fips_cntry,
 	CASE
-		WHEN data.data IS NOT NULL and paysannees.annee >= tab.begin and
-			paysannees.annee <= tab.end then CAST (data as text)
-		WHEN paysannees.ccode IS NOT NULL  and paysannees.annee >= tab.begin and
-			paysannees.annee <= tab.end
+		WHEN data.data IS NOT NULL and tablepaysannees.annee >= tab.begin and
+			tablepaysannees.annee <= tab.end then CAST (data as text)
+		WHEN tablepaysannees.ccode IS NOT NULL  and tablepaysannees.annee >= tab.begin and
+			tablepaysannees.annee <= tab.end
 			then 'TRUE'
 		ELSE '#'
 	END
 
-from (SELECT distinct annees.annee, world.fips_cntry || world.begin || world.end  as ccode,
+from (SELECT distinct annees.annee, world.ccode,
 world.begin, world.end
         FROM annees, world
         WHERE annee >= 1946 --(select min(dataset_6.var5) as min from dataset_6)
           and annee <= 2003 --(select max(dataset_6.var5) as max from dataset_6)
-        ORDER BY  annee, world.fips_cntry || world.begin || world.end) as tab
+        ORDER BY  annee, world.ccode) as tab
 
 left join (SELECT dataset_6.var2 as ccode1,world.fips_cntry,
- world.fips_cntry || world.begin || world.end as ccode,
+ world.ccode,
  dataset_6.var5 as annee,
  dataset_6.var8 as data
  FROM dataset_6, fips_cow_codes, world
@@ -46,10 +64,10 @@ left join (SELECT dataset_6.var2 as ccode1,world.fips_cntry,
  and (dataset_6.var5 >= world.begin
  and dataset_6.var5 <= world.end)
  ORDER BY dataset_6.var5,
- world.fips_cntry || world.begin || world.end) as data
+ world.ccode) as data
 		ON (tab.annee = data.annee and tab.ccode=data.ccode)
-	     left join paysannees ON (tab.ccode=paysannees.ccode
-		and tab.annee = paysannees.annee)
+	     left join tablepaysannees ON (tab.ccode=tablepaysannees.ccode
+		and tab.annee = tablepaysannees.annee)
 
 ORDER BY tab.ccode, tab.annee, ccode
 ;
