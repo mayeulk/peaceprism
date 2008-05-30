39260a0584f1fcf9b0f21a2780bbd637251cf07b
gtokarski <guytokarski@gmail.com>
2008-05-30 13:54:08 +0000

    nettoyage de la requete 'leftjoin_postgis2json' pour obtenir le nombre juste d'enrestrements: on ne classe que par annee et code-pays-annee, non par fips_cntry

--- carte_curseur_v1/script/left_join_postgis2json.sql
+++ carte_curseur_v1/script/left_join_postgis2json.sql
@@ -2,32 +2,31 @@ DROP VIEW tab, data, paysannees
 ;
 
 CREATE OR REPLACE VIEW tab AS (
-SELECT distinct annee, world.fips_cntry, world.fips_cntry || world.begin || world.end  as ccode,
+SELECT distinct annees.annee, world.fips_cntry || world.begin || world.end  as ccode,
 world.begin, world.end
         FROM annees, world
-        WHERE annee >= (select min(dataset_1.var5) as min from dataset_1)
-          and annee <= (select max(dataset_1.var5) as max from dataset_1)
+        WHERE annee >= (select min(dataset_6.var5) as min from dataset_6)
+          and annee <= (select max(dataset_6.var5) as max from dataset_6)
         ORDER BY  annee, world.fips_cntry || world.begin || world.end
 );
 
 CREATE OR REPLACE VIEW data AS (
-SELECT dataset_1.var2 as ccode1,world.fips_cntry,
+SELECT dataset_6.var2 as ccode1,world.fips_cntry,
  world.fips_cntry || world.begin || world.end as ccode,
- dataset_1.var5 as annee,
- dataset_1.var8 as data
- FROM dataset_1, fips_cow_codes, world
- WHERE dataset_1.var5 < 2004
- and (dataset_1.var2 = fips_cow_codes.cowcode)
+ dataset_6.var5 as annee,
+ dataset_6.var8 as data
+ FROM dataset_6, fips_cow_codes, world
+ WHERE  (dataset_6.var2 = fips_cow_codes.cowcode)
  and (fips_cow_codes.fips_cntry = world.fips_cntry)
- and (dataset_1.var5 >= world.begin
- and dataset_1.var5 <= world.end)
- ORDER BY dataset_1.var5,
+ and (dataset_6.var5 >= world.begin
+ and dataset_6.var5 <= world.end)
+ ORDER BY dataset_6.var5,
  world.fips_cntry || world.begin || world.end
 );
 
 CREATE OR REPLACE VIEW paysannees AS
 (
- SELECT fips_cntry, annee 
+ SELECT world.fips_cntry || world.begin || world.end as ccode, annee 
  FROM world, annees
  WHERE annee >= world.begin
  and annee <= world.end
@@ -39,7 +38,7 @@ SELECT tab.*, data.data, -- paysannees.fips_cntry,
 	CASE
 		WHEN data.data IS NOT NULL and paysannees.annee >= tab.begin and
 			paysannees.annee <= tab.end then CAST (data as text)
-		WHEN paysannees.fips_cntry IS NOT NULL  and paysannees.annee >= tab.begin and
+		WHEN paysannees.ccode IS NOT NULL  and paysannees.annee >= tab.begin and
 			paysannees.annee <= tab.end
 			then 'TRUE'
 		ELSE '#'
@@ -47,10 +46,10 @@ SELECT tab.*, data.data, -- paysannees.fips_cntry,
 	--as datajson
 	FROM tab
 	     left join data
-		ON (tab.annee = data.annee and tab.fips_cntry=data.fips_cntry)
-	     left join paysannees ON (tab.fips_cntry=paysannees.fips_cntry
+		ON (tab.annee = data.annee and tab.ccode=data.ccode)
+	     left join paysannees ON (tab.ccode=paysannees.ccode
 		and tab.annee = paysannees.annee)
 	
 --where tab.fips_cntry IN ('SB','GE', 'GM', 'GW', 'VN', 'VM','RS','UR' ) --('VM')
-ORDER BY tab.fips_cntry, tab.annee, ccode
+ORDER BY tab.ccode, tab.annee, ccode
 ;
