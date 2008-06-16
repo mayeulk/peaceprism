0b117924c4712cef01c6ea1778616475b69feed1
gtokarski <guytokarski@gmail.com>
2008-06-16 08:58:44 +0000

    premier essai d'une requete Ajax coté javascript (XMLHttpRequest) : appel dans 'refresh_var.rhtml' et utilisation dans 'json_ajax.js'

---
+++ carte_curseur_v1/public/javascripts/src/json_ajax.js
@@ -0,0 +1,68 @@
+/**
+ * @author guy
+ */
+function request01(f){
+	var liste1 = f.elements["datasets2"];
+	var liste2 = f.elements["variable2"];
+	var datChoisi = liste1.value;
+	var varChoisie = liste2.value;
+	
+	var xhr_object = null;
+	if(window.XMLHttpRequest)// firefox
+		xhr_object = new XMLHttpRequest();
+	else if(window.ActiveXObject)//IE
+		xhr_object = new ActiveXObject("Microsoft.XMLHTTP");
+	else { // XMLHttpRequest non supporté par le navigateur
+		alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest...");
+		return;
+	}
+	xhr_object.open("GET", "/json/data6-8.json", true);
+	
+	xhr_object.onreadystatechange = function() { 
+	    if(xhr_object.readyState == 4) 
+		alert(datChoisi + varChoisie);
+	         //eval(xhr_object.responseText); 
+	    } 
+		
+	//alert(datChoisi + varChoisie);
+	
+}
+
+
+ function requestjson(dataset, variable){
+ 	var xhr_object = null;
+ 	
+ 	if (window.XMLHttpRequest) // Firefox 
+			xhr_object = new XMLHttpRequest();
+		else 
+			if (window.ActiveXObject) // Internet Explorer 
+				xhr_object = new ActiveXObject("Microsoft.XMLHTTP");
+			else { // XMLHttpRequest non supporté par le navigateur 
+				alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest...");
+				return;
+			}
+		
+		
+		var filename = 'essai.js';
+		var data = null;
+		
+		
+		data = null;
+		
+		
+		xhr_object.open("GET", filename, true);
+		
+		xhr_object.onreadystatechange = function(){
+			if (xhr_object.readyState == 4) {
+				alert("essai");
+				eval('tab=' + xhr_object.responseText); 
+				//var tab = xhr_object.responseText.evalJSON();
+			}
+		}
+		
+		xhr_object.setRequestHeader("Cache-Control","public");
+		xhr_object.send(data);
+		
+		
+	}
+ 
