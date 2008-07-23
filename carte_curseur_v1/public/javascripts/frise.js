720a1e38d5a527c5de50c906c7e520754fda72e7
gtokarski <gtokarski@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-07-23 09:19:38 +0000

    amelioration du code pour la discretisation et l'affichege de la frise

--- carte_curseur_v1/public/javascripts/frise.js
+++ carte_curseur_v1/public/javascripts/frise.js
@@ -3,64 +3,64 @@
  */
 
 function afficherFrise(actuPays){
-	//alert('ici');
-	var info = tab['info'];
-	var ann = tab['annees'];
-	var pays = tab['pays'];
-	var data = tab['data'];
+	if (tab != "") {
 	
-	var long_annee = 10;
 	
-	//var actuPays = $('pays2').value ;
-	var p = 0 
-	while (pays[p] != actuPays){
-		p += 1 ;
-	}
-	
-	var tabDataPays = new Array() ;
-	var ch = '' ;
-	var coul = '';
-	var sign = '';
-	var c = 0 ;
-	
-	for (var a=0; a<ann.length; a++){
-		tabDataPays[c] = (data[parseInt(ann[a])])[p] ;
-		//alert(tabDataPays[c]);
+		var info = tab['info'];
+		var ann = tab['annees'];
+		var pays = tab['pays'];
+		var data = tab['data'];
 		
-		if (tabDataPays[c] == '#') {
-			coul = 'FFFFFF';
-			sign = 'Country dont exist';
+		var long_annee = 10;
+		
+		//var actuPays = $('pays2').value ;
+		var p = 0
+		while (pays[p] != actuPays) {
+			p += 1;
 		}
-		else {
-			if ((tabDataPays[c] == 'null') || (tabDataPays[c] == null)){
-				coul = 'C3C3C3' ;
-				sign = 'No Data' ;
-			}
-			else{
 		
-				for (y = 1; y < discretize.length; y++) {
+		var tabDataPays = new Array();
+		var ch = '';
+		var coul = '';
+		var sign = '';
+		var c = 0;
+		
+		for (var a = 0; a < ann.length; a++) {
+			tabDataPays[c] = (data[parseInt(ann[a])])[p];
+			//alert(tabDataPays[c]);
 			
-					info = discretize[y] ;
-					if (info['isFirstValue'] == 1){
-						if ((tabDataPays[c] <= info['maxi'])&&(tabDataPays[c] >= info['mini'])){
-							coul = info['couleur'];
-							sign = info['signification'];
+			if (tabDataPays[c] == '#') {
+				coul = 'FFFFFF';
+				sign = 'Country dont exist';
+			}
+			else {
+				if ((tabDataPays[c] == 'null') || (tabDataPays[c] == null)) {
+					coul = 'C3C3C3';
+					sign = 'No Data';
+				}
+				else {
+					for (var y = 1; y < discretize.length; y++) {
+						var info2 = discretize[y];
+						if (info2['isFirstValue'] == 1) {
+							if ((parseInt(tabDataPays[c]) <= parseInt(info2['maxi'])) && (parseInt(tabDataPays[c]) >= parseInt(info2['mini']))) {
+								coul = info2['couleur'];
+								sign = info2['signification'];
+							}
+						}
+						else {
+							if ((parseInt(tabDataPays[c]) <= parseInt(info2['maxi'])) && (parseInt(tabDataPays[c]) > parseInt(info2['mini']))) {
+								coul = info2['couleur'];
+								sign = info2['signification'];
+							}
 						}
 					}
-					else{
-						if ((tabDataPays[c] <= info['maxi'])&&(tabDataPays[c] > info['mini'])){
-							coul = info['couleur'];
-							sign = info['signification'];
-						}
-					}			
 				}
 			}
+			ch += '<div title="' + ann[a] + ' : ' + sign + '(' + tabDataPays[c] + ')" style="height:20px; width:' + long_annee + 'px;background-color:#' + coul + ';float:left"> </div>';
+			c += 1;
 		}
-		ch += '<div title="' + ann[a] + ' : ' + sign + '(' + tabDataPays[c] + ')" style="height:20px; width:' + long_annee + 'px;background-color:#' + coul + ';float:left"> </div>' ;
-		c +=1 ;
+		//alert(tabDataPays);
+		$('frise').innerHTML = ch;
+		
 	}
-	//alert(tabDataPays);
-	$('frise').innerHTML = ch ;
-	
-	
 }
