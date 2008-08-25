9e834ab65eb371b07ab2932a8c6eed2ddc8a8ca7
gtokarski <gtokarski@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-08-25 16:15:36 +0000

    ajout des tests unitaires pour le controlleur 'carte_controller'

--- carte_curseur_v1/test/functional/carte_controller_test.rb
+++ carte_curseur_v1/test/functional/carte_controller_test.rb
@@ -15,23 +15,43 @@ class CarteControllerTest < ActionController::TestCase
     @response   = ActionController::TestResponse.new
   end
 
+  # test verifiant la selection du browser
   def test_should_get_list_with_xml
     @request.headers['HTTP_ACCEPT'] = "application/xhtml+xml"
     get :list
+    assert_response :success
     assert_not_nil assigns(:browser)
     assert_equal "moz", assigns(:browser)
 #    assert_response :success
     assert_equal "application/xhtml+xml; charset=utf-8", @response.headers['type']
   end
 
+  # test verifiant la selection du browser
   def test_should_get_list_with_vml
     @request.headers['HTTP_ACCEPT'] = "*/*"
     get :list
+    assert_response :success
     assert_not_nil assigns(:browser)
     assert_equal "ie", assigns(:browser)
   end
 
+  def test_should_redirect_index_to_list_xml
+    @request.headers['HTTP_ACCEPT'] = "application/xhtml+xml"
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:browser)
+    assert_equal "moz", assigns(:browser)
+#    assert_response :success
+    assert_equal "application/xhtml+xml; charset=utf-8", @response.headers['type']
+  end
 
+  def test_should_redirect_index_to_list_vml
+    @request.headers['HTTP_ACCEPT'] = "*/*"
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:browser)
+    assert_equal "ie", assigns(:browser)
+  end
 
 
 
@@ -41,9 +61,39 @@ class CarteControllerTest < ActionController::TestCase
 #    assert_equal 2, assigns(@dat).size
 #  end
   def test_refresh_var
+    
     get :refresh_var, :datas => 1
     assert_response :success
+    assert_equal 4, assigns(:var_select).size
+    assert_equal 17, assigns(:citation).size
+    assert_equal 5, assigns(:vars).size
+    assert_equal 2, assigns(:datasets2).size
+    assert_equal 2, assigns(:datasets).size
+    assert_equal 3, assigns(:dat).size
   end
+
+  def test_show_over_conflicts_zone
+    get :show_over, :id => "lt35lg110rd2000", :dataset => 1, :variable => 1
+    assert_response :success
+    assert_equal 1, assigns(:dataset).size
+    assert_equal 1, assigns(:variable).size
+    assert_equal 1, assigns(:conflitsexts).size
+  end
+
+  def test_show_over_country
+    get :show_over, :id => 'FR19462003', :dataset => 1, :variable => 8
+    assert_response :success
+    assert_equal 1946, assigns(:debut_periode)
+    assert_equal 1, assigns(:dataset).size
+    assert_equal 1, assigns(:variable).size
+    assert_nil assigns(:conflitsexts)
+    assert_equal 'FR19462003', assigns(:id)
+    assert_equal "220", assigns(:cow_code)
+    assert_equal '2', assigns(:identifierccode1_var)
+    assert_equal '3', assigns(:identifieryear_var)
+    assert_equal 3, assigns(:donnees_frise).size
+  end
+
 
 #  def test_index
 #    get :index
