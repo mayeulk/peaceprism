78e52603c9ed283ffa38bf309fb35ca418f8cd45
Mayeul Kauffmann <mayeulk@users.noreply.github.com>
2008-08-26 16:19:06 +0000

    * Suite de la revision r219 (index double dans la table variables)  * Documentation de l'usage des migrations

--- carte_curseur_v1/db/migrate/009_add_variable_index.rb
+++ carte_curseur_v1/db/migrate/009_add_variable_index.rb
@@ -1,9 +1,6 @@
 class AddVariableIndex < ActiveRecord::Migration
   def self.up
-    add_index(:variables, [:var_id, :dataset_id], :unique => true, :name => 'var_id_dataset_id_index') 
-#    add_index(:accounts, [:branch_id, :party_id], :unique => true, :name => 'by_branch_party')
-
-    # , :unique => true)
+    add_index(:variables, [:var_id, :dataset_id], :unique => true) 
   end
 
   def self.down
