773734b02480a4a06331c0771945d4ff41914a6c
Mayeul Kauffmann <mayeulk@users.noreply.github.com>
2008-08-27 15:31:25 +0000

    Correctif a la revison r223 : Le code "fixtures :variables, :datasets " doit se trouver dans la definition de la class VariableTest, et non avant

--- carte_curseur_v1/test/unit/variable_test.rb
+++ carte_curseur_v1/test/unit/variable_test.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-fixtures :variables, :datasets
 
 class VariableTest < ActiveSupport::TestCase
+  fixtures :variables, :datasets
   # Replace this with your real tests.
   def test_truth
     assert true
