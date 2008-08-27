70021f102a68fa443910e3f4e84236b2ee6a9043
gtokarski <guytokarski@gmail.com>
2008-08-27 13:41:01 +0000

    correction du bugg sur la selection d'un dataset lors de l'actualisation de la page : - on force la valeur '0'
    ticket : Tickets: #5
    
    ... apparition d'un bugg sur les tests unitaires ...

--- carte_curseur_v1/test/functional/carte_controller_test.rb
+++ carte_curseur_v1/test/functional/carte_controller_test.rb
@@ -7,7 +7,7 @@ class CarteController; def rescue_action(e) raise e end; end
 #class CarteControllerTest < Test::Unit::TestCase
 
 class CarteControllerTest < ActionController::TestCase
-  fixtures :conflitsexts, :datasets, :variables, :var_labels, :fips_cow_codes
+  fixtures :conflitsexts, :datasets, :variables, :var_labels, :fips_cow_codes, :dataset_1, :dataset_2
 
   def setup
     @controller = CarteController.new
@@ -15,7 +15,7 @@ class CarteControllerTest < ActionController::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  # test verifiant la selection du browser
+  # tests verifiant la selection du browser
   def test_should_get_list_with_xml
     @request.headers['HTTP_ACCEPT'] = "application/xhtml+xml"
     get :list
@@ -26,7 +26,6 @@ class CarteControllerTest < ActionController::TestCase
     assert_equal "application/xhtml+xml; charset=utf-8", @response.headers['type']
   end
 
-  # test verifiant la selection du browser
   def test_should_get_list_with_vml
     @request.headers['HTTP_ACCEPT'] = "*/*"
     get :list
@@ -35,6 +34,7 @@ class CarteControllerTest < ActionController::TestCase
     assert_equal "ie", assigns(:browser)
   end
 
+  # tests verifiant la redirection en fonction des url
   def test_should_redirect_index_to_list_xml
     @request.headers['HTTP_ACCEPT'] = "application/xhtml+xml"
     get :index
@@ -53,13 +53,8 @@ class CarteControllerTest < ActionController::TestCase
     assert_equal "ie", assigns(:browser)
   end
 
+  # tests sur la bonne affectation des variables
 
-
-
-#  def test_datasets_are_assigned_properly
-#    get :index
-#    assert_equal 2, assigns(@dat).size
-#  end
   def test_refresh_var
     
     get :refresh_var, :datas => 1
@@ -94,78 +89,13 @@ class CarteControllerTest < ActionController::TestCase
     assert_equal 3, assigns(:donnees_frise).size
   end
 
+  # tests verifiant les vues rendues
+  def test_good_number_of_datasets_in_select_box
+    @request.headers['HTTP_ACCEPT'] = "application/xhtml+xml"
+    get :index
+    assert_select "select", "Please choose a dataset\ndataset1\ndataset2"
+    assert_select "select option", "Please choose a dataset"
+  end
 
-#  def test_index
-#    get :index
-#    #assert_response :success
-#    assert_response :missing
-#    #assert_template 'list'
-#  end
 
-#  def test_list
-#    get :list
-#
-#    assert_response :success
-#    assert_template 'list'
-#
-#    assert_not_nil assigns(:conflitsexts)
-#  end
-
-#  def test_show
-#    get :show, :id => 1
-#
-#    assert_response :success
-#    assert_template 'show'
-#
-#    assert_not_nil assigns(:conflitsext)
-#    assert assigns(:conflitsext).valid?
-#  end
-#
-#  def test_new
-#    get :new
-#
-#    assert_response :success
-#    assert_template 'new'
-#
-#    assert_not_nil assigns(:conflitsext)
-#  end
-
-#  def test_create
-#    num_conflitsexts = Conflitsext.count
-#
-#    post :create, :conflitsext => {}
-#
-#    assert_response :redirect
-#    assert_redirected_to :action => 'list'
-#
-#    assert_equal num_conflitsexts + 1, Conflitsext.count
-#  end
-
-#  def test_edit
-#    get :edit, :id => 1
-#
-#    assert_response :success
-#    assert_template 'edit'
-#
-#    assert_not_nil assigns(:conflitsext)
-#    assert assigns(:conflitsext).valid?
-#  end
-
-#  def test_update
-#    post :update, :id => 1
-#    assert_response :redirect
-#    assert_redirected_to :action => 'show', :id => 1
-#  end
-#
-#  def test_destroy
-#    assert_not_nil Conflitsext.find(1)
-#
-#    post :destroy, :id => 1
-#    assert_response :redirect
-#    assert_redirected_to :action => 'list'
-#
-#    assert_raise(ActiveRecord::RecordNotFound) {
-#      Conflitsext.find(1)
-#    }
-#  end
 end
