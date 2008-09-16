4e8563347210f4fc9a68183e54343d97db7f4eaa
gtokarski <gtokarski@7cf5c76d-91fa-4b42-8f2d-73a4eb11a52e>
2008-09-16 14:32:54 +0000

    repositionnement des icones de navigation, au dessus et autour de la carte. ticket:3

--- carte_curseur_v1/public/javascripts/modifier_svg_moz.js
+++ carte_curseur_v1/public/javascripts/modifier_svg_moz.js
@@ -18,3 +18,98 @@ function masquerPays(pays){
 
 //paysSVG.setAttribute("opacity", "0.5");
 //truc.style.visibility = 'visible';
+
+rsz=0;
+carte_w=600;
+carte_h=300;
+function resz() { 
+	if (rsz==0)     {
+		rsz=1; 
+		//document.getElementById("navig").style.backgroundColor="red";
+		document.getElementById("cadre_carte").className="cadre_carte_rouge";
+		document.getElementById("b").src="images/icones/b-r.png";
+		document.getElementById("bd").src="images/icones/bd-r.png";
+		document.getElementById("bg").src="images/icones/bg-r.png";
+		document.getElementById("d").src="images/icones/d-r.png";
+		document.getElementById("g").src="images/icones/g-r.png";
+		document.getElementById("h").src="images/icones/h-r.png";
+		document.getElementById("hd").src="images/icones/hd-r.png";
+		document.getElementById("hg").src="images/icones/hg-r.png";
+		//document.getElementById("resize").src="images/icones/resize_enfonce.png";
+		
+	}
+	else {
+		rsz=0;
+		//    document.getElementById("navig").style.backgroundColor="white";
+		document.getElementById("cadre_carte").className="cadre_carte";    
+		document.getElementById("b").src="images/icones/b.png";
+		document.getElementById("bd").src="images/icones/bd.png";
+		document.getElementById("bg").src="images/icones/bg.png";
+		document.getElementById("d").src="images/icones/d.png";
+		document.getElementById("g").src="images/icones/g.png";
+		document.getElementById("h").src="images/icones/h.png";
+		document.getElementById("hd").src="images/icones/hd.png";
+		document.getElementById("hg").src="images/icones/hg.png";
+		//document.getElementById("resize").src="images/icones/resize_enfonce.png";
+	}
+}
+//x_org = 60; //milieu d'origine (x)
+//y_org = -10 ;//milieu d'origine (y)
+x_org = 0; //milieu d'origine (x)
+y_org = 0; //milieu d'origine (y)
+//w_org = 240 ;//largeur d'origine (???) (sino zoombox incorrecte???!!)
+//h_org = 160 ;//hauteur d'origine
+//n_x=-130;
+//n_y=-80;
+w_org=3600;
+h_org=1800;
+
+
+function initzoom()
+{
+	document.getElementById("monde").setAttributeNS(null,"viewBox","-1800 -900 3600 1800");
+	x_org = 0 ;//milieu d'origine (x)
+	y_org = 0 ;//milieu d'origine (y)
+	w_org=3600;
+	h_org=1800;
+	
+}
+
+function pan(dx,dy)
+{
+	if (rsz==1) {
+		carte_w=carte_w+dx*400;
+		carte_h=carte_h+dy*400;
+		if (30>carte_w) {carte_w=30}; //ne pas changer la taille si elle est deja� petite
+		if (30>carte_h) {carte_h=30}; //NB: "30>" marche mais pas "carte_h inferieur_a� 30" car le parseur xml le prend pour une balise  
+		document.getElementById("monde").setAttributeNS(null,"width",carte_w);
+		document.getElementById("monde").setAttributeNS(null,"height",carte_h);
+		document.getElementById("cadre_carte").style.width=carte_w+"px";
+		document.getElementById("cadre_carte").style.height=carte_h+"px";		
+	}
+	else
+	{
+		x_org=x_org+dx*(w_org+h_org);
+		y_org=y_org+dy*(w_org+h_org);
+		n_x=x_org-w_org/2;
+		n_y=y_org-h_org/2;
+		new_vb = n_x + " " + n_y + " " + w_org + " " + h_org;
+		document.getElementById("monde").setAttributeNS(null,"viewBox",new_vb);
+		//document.getElementById("nompays").value="x_org:"+x_org+" y_org:"+y_org+" nx:"+n_x+" ny:"+n_y+" w"+w_org+" h"+h_org
+		//document.getElementById("nompays").value=new_vb		
+	}
+}
+
+// fonction de Zoom :
+function zoom(facteur)
+{
+	// en SVG (pour firefox), il faut modifier les parametres de la viewbox associee au groupe "monde"
+	w_org=w_org/facteur;
+	h_org=h_org/facteur;
+	//w_org = w_org*facteur;
+	//h_org = h_org*facteur;
+	n_x=x_org-w_org/2;
+	n_y=y_org-h_org/2;	
+	new_vb = n_x + " " + n_y + " " + w_org + " " + h_org;
+	document.getElementById("monde").setAttributeNS(null,"viewBox",new_vb);
+}
--- carte_curseur_v1/public/javascripts/modifier_vml_ie.js
+++ carte_curseur_v1/public/javascripts/modifier_vml_ie.js
@@ -16,4 +16,100 @@ function masquerPays(pays){
 	pays.style.visibility = 'hidden';
 }
 
-//paysSVG.setAttribute("opacity", "0.5");
+rsz=0;
+carte_w=600;
+carte_h=300;
+function resz() { 
+	if (rsz==0)     {
+		rsz=1; 
+		//document.getElementById("navig").style.backgroundColor="red";
+		document.getElementById("cadre_carte").className="cadre_carte_rouge";
+		document.getElementById("b").src="images/navig/b-r.gif";
+		document.getElementById("bd").src="images/navig/bd-r.gif";
+		document.getElementById("bg").src="images/navig/bg-r.gif";
+		document.getElementById("d").src="images/navig/d-r.gif";
+		document.getElementById("g").src="images/navig/g-r.gif";
+		document.getElementById("h").src="images/navig/h-r.gif";
+		document.getElementById("hd").src="images/navig/hd-r.gif";
+		document.getElementById("hg").src="images/navig/hg-r.gif";
+		document.getElementById("resize").src="images/navig/resize_enfonce.gif";
+		
+	}
+	else {
+		rsz=0;
+		//    document.getElementById("navig").style.backgroundColor="white";
+		document.getElementById("cadre_carte").className="cadre_carte";    
+		document.getElementById("b").src="images/navig/b.gif";
+		document.getElementById("bd").src="images/navig/bd.gif";
+		document.getElementById("bg").src="images/navig/bg.gif";
+		document.getElementById("d").src="images/navig/d.gif";
+		document.getElementById("g").src="images/navig/g.gif";
+		document.getElementById("h").src="images/navig/h.gif";
+		document.getElementById("hd").src="images/navig/hd.gif";
+		document.getElementById("hg").src="images/navig/hg.gif";
+		document.getElementById("resize").src="images/navig/resize.gif";
+	}
+}
+
+x_org = 0 ;//milieu d'origine (x) (bof)
+y_org = 0 ;//milieu d'origine (y) (bof)
+w_org = 600 ;//largeur d'origine (???) (sino zoombox incorrecte???!!)
+h_org = 300 ;//hauteur d'origine
+n_x=0;
+n_y=0;
+
+function initzoom()
+{
+	x_org = 0 ;//milieu d'origine (x) (bof)
+	y_org = 0; //milieu d'origine (y) (bof)
+	w_org = 600; //largeur d'origine (???) (sino zoombox incorrecte???!!)
+	h_org = 300; //hauteur d'origine
+	n_x=0;
+	n_y=0;
+	document.getElementById("monde").style.left=n_x+"px";
+	document.getElementById("monde").style.top=n_y+"px";
+	document.getElementById("monde").style.width=w_org+"px";
+	document.getElementById("monde").style.height=h_org+"px";
+}
+
+function pan(dx,dy)
+{
+	if (rsz==1) {		
+		carte_w=carte_w+dx*400 ;
+		carte_h=carte_h+dy*400 ;
+		if (30>carte_w) {carte_w=30}; //ne pas changer la taille si elle est deja� petite
+		if (30>carte_h) {carte_h=30};
+		
+		document.getElementById("cadre_carte").style.width=carte_w+"px";
+		document.getElementById("cadre_carte").style.height=carte_h+"px";
+	}
+	else
+	{
+	n_x=n_x-dx*(w_org);
+	n_y=n_y-dy*(w_org);
+	document.getElementById("monde").style.left=n_x+"px";
+	document.getElementById("monde").style.top=n_y+"px";
+	//document.getElementById("nompays").value="nx:"+n_x+" ny:"+n_y+" w"+w_org+" h"+h_org
+	}
+}
+
+// fonction de Zoom :
+function zoom(facteur)
+{
+	// en VML, il faut modifier les attributs width et height du groupe "monde"
+	w_org=Math.round(w_org*facteur);
+	h_org=Math.round(h_org*facteur);
+	
+	n_x=Math.round(facteur*n_x+(1-facteur)*carte_w/2);
+	n_y=Math.round(facteur*n_y+(1-facteur)*carte_h/2);
+	//n_x = Math.round(n_x + carte_w/2 + 1/facteur*carte_w)
+	//n_y = Math.round(n_y + carte_h/2 + 1/facteur*carte_h)
+	
+	document.getElementById("monde").style.left=n_x+"px";
+	document.getElementById("monde").style.top=n_y+"px";
+	document.getElementById("monde").style.width=w_org+"px";
+	document.getElementById("monde").style.height=h_org+"px";
+	//document.getElementById("monde").coordorigin=n_x+","+n_y;
+	//document.getElementById("monde").coordsize=w_org+","+h_org;
+}
+
