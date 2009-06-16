6978bc794bd62028eef9519c14a1c2647864ca84
Mayeul Kauffmann <mayeulk@users.noreply.github.com>
2009-06-16 15:27:36 +0000

    test vdf reintegratio polity4

--- carte_curseur_v1/script/sauvegarde_postgresql.sh
+++ carte_curseur_v1/script/sauvegarde_postgresql.sh
@@ -2,10 +2,18 @@
 
 maintenant=`date +%Y%m%d-%T`
 
-fichier='/home/mk/svg_ecole/postgresql/'$maintenant'postgresql.sql.gz'
+#fichier='/home/mk/svg_ecole/postgresql/'$maintenant'postgresql.sql.gz'
+#fichier_sql='/home/mk/svg_ecole/postgresql/'$maintenant'postgresql.sql'
+fichier_tar='/home/mk/svg_ecole/postgresql/'$maintenant'postgresql.tar'
+#fichier_tar_gz='/home/mk/svg_ecole/postgresql/'$maintenant'postgresql.tar.gz'
 
-echo 'Sauvegarde en cours dans '$fichier
+echo 'Sauvegarde en cours dans '$fichier_tar
 
-pg_dump --compress=2 --username=postgres --port=5433 testgis > $fichier
+# pg_dump --compress=2 --username=postgres --port=5433 prism_db > $fichier
+# pg_dump --create --format=plain --username=postgres --port=5433 prism_db > $fichier_sql
+pg_dump --format=tar --username=postgres --port=5433 prism_db > $fichier_tar
+# pg_dump --format=custom --username=postgres --port=5433 prism_db > $fichier_tar_gz
 
 echo 'Sauvegarde terminée'
+
+echo 'pour restorer, faire: pg_restore --create --dbname=postgres prism_db.tar'
